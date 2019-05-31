a278f0da9789aa52cd6549d4ebdc767354163832
Jasper Cheung <wymanjasper@gmail.com>
2019-05-31 11:56:16 -0400

    click color done

--- script.js
+++ script.js
@@ -3,73 +3,74 @@ let colCount = 16;
 let cellsTable = document.getElementById("cells");
 
 function rowPlus() {
-    let newRow = document.createElement("tr");
-    for (let i = 0; i < colCount; i++) {
-        let newCell = document.createElement("td");
-        newRow.appendChild(newCell);
-    }
-    cellsTable.appendChild(newRow);
-    rowCount++;
+  let newRow = document.createElement("tr");
+  for (let i = 0; i < colCount; i++) {
+    let newCell = document.createElement("td");
+    newCell.addEventListener("click",colorCell);
+    newRow.appendChild(newCell);
+  }
+  cellsTable.appendChild(newRow);
+  rowCount++;
 };
 
 function rowMinus() {
-    cellsTable.removeChild(cellsTable.lastChild);
-    rowCount--;
+  cellsTable.removeChild(cellsTable.lastChild);
+  rowCount--;
 };
 
 function colPlus() {
-    let children = Array.from(cellsTable.children);
-    children.forEach((row) => {
-        let newCell = document.createElement("td");
-        row.appendChild(newCell);
-    });
-    colCount++;
+  let children = Array.from(cellsTable.children);
+  children.forEach((row) => {
+    let newCell = document.createElement("td");
+    newCell.addEventListener("click",colorCell);
+    row.appendChild(newCell);
+  });
+  colCount++;
 };
 
 function colMinus() {
-    let children = Array.from(cellsTable.children);
-    children.forEach((row) => {
-        row.removeChild(row.lastChild);
-    });
-    colCount--;
+  let children = Array.from(cellsTable.children);
+  children.forEach((row) => {
+    row.removeChild(row.lastChild);
+  });
+  colCount--;
 };
 
 let colorSelector = document.getElementById("select-color");
 let color;
 function updateColor() {
-    color = colorSelector.value;
+  color = colorSelector.value;
 };
 updateColor();
 
 function fillAll(){
   let tableChildren = Array.from(cellsTable.children);
-    tableChildren.forEach((row) => {
-      let rowChildren = Array.from(row.children);
-      rowChildren.forEach((cell)=>{
-        cell.style.backgroundColor =color;
+  tableChildren.forEach((row) => {
+    let rowChildren = Array.from(row.children);
+    rowChildren.forEach((cell)=>{
+      cell.style.backgroundColor =color;
 
-      });
     });
+  });
 };
 
 function fillEmpty(){
   let tableChildren = Array.from(cellsTable.children);
-    tableChildren.forEach((row) => {
-      let rowChildren = Array.from(row.children);
-      rowChildren.forEach((cell)=>{
+  tableChildren.forEach((row) => {
+    let rowChildren = Array.from(row.children);
+    rowChildren.forEach((cell)=>{
 
-        if(cell.style.backgroundColor == ""){
+      if(cell.style.backgroundColor == ""){
 
-          cell.style.backgroundColor =color;
-        }
-        //console.log(cell.style.backgroundColor);
+        cell.style.backgroundColor =color;
+      }
+      //console.log(cell.style.backgroundColor);
 
-      });
     });
+  });
 };
 
 function clearCells(){
-
   let tableChildren = Array.from(cellsTable.children);
   tableChildren.forEach((row) => {
     let rowChildren = Array.from(row.children);
@@ -78,4 +79,9 @@ function clearCells(){
 
     });
   });
+};
+
+function colorCell(){
+  console.log(this);
+  this.style.backgroundColor = color;
 };
