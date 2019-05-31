951fb9a1d4e8a52808b4bbe6cf042c8be06286c4
srafi1 <shakilrafi0@gmail.com>
2019-05-31 11:17:29 -0400

    adding rows

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-let ok = "ok";
+let rowCount = 0;
+let colCount = 16;
+let cellsTable = document.getElementById("cells");
+
+function rowPlus() {
+    let newRow = document.createElement("tr");
+    for (let i = 0; i < colCount; i++) {
+        let newCell = document.createElement("td");
+        newRow.appendChild(newCell);
+    }
+    cellsTable.appendChild(newRow);
+    rowCount++;
+}
+
+let colorSelector = document.getElementById("select-color");
+let color;
+function updateColor() {
+    color = colorSelector.value;
+}
+updateColor();
